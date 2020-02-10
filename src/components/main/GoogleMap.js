1375175e23fd45305b2e9ebf29dabf1dd8a1808f
Eric Huang <797564@ACDM.DS.SAIT.CA>
2020-02-10 12:31:19 -0700

    Top10Product, Regions implemented in Google Maps

--- src/components/main/GoogleMap.js
+++ src/components/main/GoogleMap.js
@@ -1,20 +1,56 @@
-import React, { useEffect, useRef, createRef } from 'react';
+import React, { Component, createRef } from "react";
+import top10Products from '../../data/topTenProducts.json';
 
+import Regions from '../../data/Regions.json';
 
 const mapStyles = {
-    width: "100vw", 
+	width: "100vw",
 	height: "100vh",
+	// zIndex: -1,
 };
 
-function GoogleMap(props) {
-    const googleMapRef = createRef();
-    const googleMap = useRef(null);
-    const marker = useRef(null);
+var markers = [];
+var polygons = [];
 
-    const createGoogleMap = () =>
-        new window.google.maps.Map(googleMapRef.current, {
-			center: { 
-				lat: 59.8478, lng: -101.8939 
+
+class GoogleMap extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		}
+		this.googleMapRef = createRef();
+
+	}
+	componentDidMount() {
+		const googleMapScript = document.createElement("script");
+		googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&language=${this.props.language}`;
+		window.document.body.appendChild(googleMapScript);
+
+		this.markers = [];
+		//getTop10Products(); //this will fetch the top10Products, may have "wait" for data before displaying
+
+		googleMapScript.addEventListener("load", () => {
+			this.createGoogleMap();
+			this.showTop10Products();
+			this.createRegions();
+		});
+		
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		return (nextProps.currentMarkers !== this.props.currentMarkers)
+	}
+
+	componentDidUpdate() {
+		console.log("Google Map Component updated!")
+		this.showMarkers(this.props.currentMarkers);
+		console.log(this.markers);
+	}
+
+	createGoogleMap = () => {
+		this.googleMap = new window.google.maps.Map(this.googleMapRef.current, {
+			center: {
+				lat: 59.8478, lng: -101.8939
 			},
 			zoom: 4,
 			zoomControl: true,
@@ -23,37 +59,134 @@ function GoogleMap(props) {
 			streetViewControl: false,
 			rotateControl: false,
 			fullscreenControl: true,
+		});
+
+	}
+
+	showTop10Products = () => {
+		this.showMarkers(top10Products);
+	}
+
+
+	showMarkers = (newMarkers) => {
+		newMarkers.map(marker => {
+			const { position, id } = marker;
+			var gMapMarker = new window.google.maps.Marker({
+				position: {
+					lat: position.lat,
+					lng: position.lng
+				},
+				label: id.toString(),
+				map: this.googleMap
+			});
+
+			gMapMarker.addListener("click", () => {
+				this.props.setSelectedMarker(gMapMarker);
+			});
+			this.markers.push(gMapMarker);
+		});
+	}
+
+	clearCurrentMarkers = () => {
+		this.props.setCurrentMarkers([]);
+		for (var i = 0; i < this.markers.length; i++) {
+			this.markers[i].setMap(null);
+		}
+		this.markers = [];
+	}
+
+	createRegions = () => {
+		var regions = Regions;
+		for (let regionsIndex = 0; regionsIndex < regions.length; regionsIndex++) {//loop every region
+			//every region
+			// console.log("region: " + regions)
+			let polygons = regions[regionsIndex].polygons;
+			for (let polygonsIndex = 0; polygonsIndex < polygons.length; polygonsIndex++) {//loop every polygons in a region
+				//every polygons
+				// console.log("polygons: " + polygons[polygonsIndex])
+				// console.log("outer: " + polygons[polygonsIndex].outerPath)
+				let polygon = polygons[polygonsIndex];
+				let polygonPaths=[];
+				let outerPath = this.convertStringToArrayCoords(polygon.outerPath);
+				polygonPaths.push(outerPath);
+				for (let innerIndex = 0; innerIndex < polygon.innerPaths.length; innerIndex++) {//loop every inner polygon
+					// console.log("inner: " + polygon.innerPaths[innerIndex].innerPath)
+					let innerPath = this.convertStringToArrayCoordsBackwards(polygon.innerPaths[innerIndex].innerPath);
+					polygonPaths.push(innerPath);
+				}
+				let regionInfo = {"regionName":regions[regionsIndex].regionName, "polygonStyle":regions[regionsIndex].polygonStyle, "polygonPaths":polygonPaths};
+				this.createRegion(regionInfo);
+			}
 			
-        });
+		}
+	}
 
-    const createMarker = () =>
-        new window.google.maps.Marker({
-            position: {
-				lat: 59.8478, 
-				lng: -101.8939
-			},
-            map: googleMap.current
-        });
+	convertStringToArrayCoords = (MultiGeometryCoordinates) => {
+		let finalData = [];
+		var grouped = MultiGeometryCoordinates.split("\n");
+		grouped.forEach(function(item, i){
+			// item = item.trim().replace(/,0/g, "");
+			// console.log(item);
+			let a = item.trim().split(',');
+	
+			finalData.push({
+				lng: parseFloat(a[0]),
+				lat: parseFloat(a[1])
+			});
+			
+		});
+		return finalData;
+	}
 
-    useEffect(() => {
-        const googleMapScript = document.createElement('script');
-        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&libraries=places`
-        window.document.body.appendChild(googleMapScript);
+	convertStringToArrayCoordsBackwards = (MultiGeometryCoordinates) => {
+		let finalData = [];
+		var grouped = MultiGeometryCoordinates.split("\n");
+		for (let index = grouped.length-1; index >= 0 ; index--) {
+			let item = grouped[index];
+			let a = item.trim().split(',');
+	
+			finalData.push({
+				lng: parseFloat(a[0]),
+				lat: parseFloat(a[1])
+			});
+			
+		}
+		return finalData;
+	}
 
-        googleMapScript.addEventListener('load', () => {
-            googleMap.current = createGoogleMap();
-            marker.current = createMarker()
-        })
-    }, []);
+	createRegion = (regionInfo) => {
+		const {strokeColor, strokeWeight, fillColor} = regionInfo.polygonStyle;
+		// console.log(newpolygons)
+	  	var polygon = new window.google.maps.Polygon({
+				paths: regionInfo.polygonPaths,
+				strokeColor: strokeColor,
+				strokeWeight : strokeWeight,
+				fillColor: fillColor,
+				map: this.googleMap,
+			});
+		polygon.addListener("click", () => {
+			//fetch
+			console.log(regionInfo.regionName)
+		});
+	  	polygons.push(polygon);
+	}	  
 
-    return (
-        <div
-            id="google-map"
-            ref={googleMapRef}
-            style={mapStyles}
-        />
-    )
+	render() {
+		return (
+			<div>
+				<div
+					ref={this.googleMapRef}
+					id="google-map"
+					style={mapStyles}
+				/>
+
+			</div>
+		);
+	}
 
 }
 
-export default GoogleMap
+	
+
+
+export default GoogleMap;
