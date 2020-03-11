765ff715122c2ada7e1aa933c0968147434eeb2c
Umar Hassan <799240@ACDM.DS.SAIT.CA>
2020-03-10 21:10:50 -0600

    stuff

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,8 +4,25 @@ import "./css/login.css";
 class LoginPage extends React.Component {
   constructor() {
     super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleChange(event) {
+    const { name, value } = event.target;
+    // const {name, value, type, checked} = event.target
+    this.setState({ [name]: [value] });
+  }
+
+  errorMessage = "";
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // if(this.state.email==""||)?
+  }
   render() {
     return (
       <div style={{ display: "flex" }}>
@@ -13,36 +30,49 @@ class LoginPage extends React.Component {
           style={{
             height: "100vh",
             width: "70%",
-            backgroundColor: "red",
+            backgroundColor: "white",
             display: "inline"
           }}
           className="d-flex justify-content-center"
         >
           <div className="loginBox d-flex justify-content-center">
             <div style={{ display: "inline-block" }}>
-              <div>
-                <label>
-                  <h5>Email</h5> &nbsp;
-                  <input
-                    type="text"
-                    style={{
-                      borderStyle: "solid",
-                      backgroundColor: "rgb(244,248,247)",
-                      marginBottom: "20px"
-                    }}
-                  />
-                  <h5>Password</h5>
-                  <input
-                    type="password"
-                    style={{
-                      borderStyle: "solid",
-                      backgroundColor: "rgb(244,248,247)",
-                      marginBottom: "20px"
-                    }}
-                  />
-                </label>
-              </div>
-              <div><button>Login</button> <button>Sign up</button></div>
+              <form onSubmit={this.handleSubmit}>
+                <div>
+                  <label>
+                    <h5>Email</h5> &nbsp;
+                    <input
+                      type="text"
+                      style={{
+                        borderStyle: "solid",
+                        backgroundColor: "rgb(244,248,247)",
+                        marginBottom: "20px"
+                      }}
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                  <br />
+                  <label>
+                    <h5>Password</h5>
+                    <input
+                      type="password"
+                      style={{
+                        borderStyle: "solid",
+                        backgroundColor: "rgb(244,248,247)",
+                        marginBottom: "20px"
+                      }}
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
+                <div>
+                  <input type="submit" value="Login" /> <button>Sign up</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
@@ -50,7 +80,7 @@ class LoginPage extends React.Component {
           style={{
             height: "100vh",
             width: "30%",
-            backgroundColor: "purple",
+            backgroundColor: "#8574e7",
             display: "inline"
           }}
           className="d-flex justify-content-center"
--- src/components/main/SearchSidebar.js
+++ src/components/main/SearchSidebar.js
@@ -1,201 +1,256 @@
-<<<<<<< HEAD
-import React, { useState, useEffect, useRef } from "react";
+// import React, { useState, useEffect, useRef } from "react";
 
-function SearchSidebar(props) {
-  //Search input box
-  let [searchText, setsearchText] = useState("");
+// function SearchSidebar(props) {
+//   //Search input box
+//   let [searchText, setsearchText] = useState("");
 
-  const inputText = useRef("");
+//   const inputText = useRef("");
 
-  /*
-		searchCategories = Fetch search options I.E Products, Season, Region. 
-			This also include the array of items I.E Products item are [Barley, Leek, Sockeye Salmon], Season: [winter, summer]
-=======
+// searchCategories = Fetch search options I.E Products, Season, Region.
+// 	This also include the array of items I.E Products item are [Barley, Leek, Sockeye Salmon], Season: [winter, summer]
+
 // import React, { useState, useEffect, useRef } from 'react';
-import React, { Component } from 'react';
-import SearchItem from './SearchItem';
+import React, { Component } from "react";
+import SearchItem from "./SearchItem";
 
 class SearchSidebar extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			productsLoading: true,
-			productsError: false,
-			regionsLoading: true,
-			regionsError: false,
-			seasonsLoading: true,
-			productTypesLoading: true,
-			searchText: "",//Search Input text box
-			products: [],
-			regions: [],
-			seasons: ["Fall", "Spring", "Summer", "Winter"],
-			productTypes: ["Seed", "Grain", "Plant", "Herb", "Fruit", "Vegetable", "Animal", "By-product", 
-				"Freshwater product", "Saltwater product", "Farmed product", "Green house product", "Aquaponic product",
-				"Sustainable", "Endangered", "Artisanal product"] 
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      productsLoading: true,
+      productsError: false,
+      regionsLoading: true,
+      regionsError: false,
+      seasonsLoading: true,
+      productTypesLoading: true,
+      searchText: "", //Search Input text box
+      products: [],
+      regions: [],
+      seasons: ["Fall", "Spring", "Summer", "Winter"],
+      productTypes: [
+        "Seed",
+        "Grain",
+        "Plant",
+        "Herb",
+        "Fruit",
+        "Vegetable",
+        "Animal",
+        "By-product",
+        "Freshwater product",
+        "Saltwater product",
+        "Farmed product",
+        "Green house product",
+        "Aquaponic product",
+        "Sustainable",
+        "Endangered",
+        "Artisanal product"
+      ]
+    };
+  }
 
-	componentDidMount() {
-		//Fetch products
-		fetch("http://localhost:8080/products")
-		 .then(response => response.json())
-		 .then(productsData => {
-			 this.setState({
-				productsLoading: false,
-				 products: productsData
-				});
-		 })
-		 .catch(() => {
-			 console.log("Failed to retrieve products for SearchSidebar");
-			 this.setState({productsError: true});
-		 });//end of fetch
+  componentDidMount() {
+    //Fetch products
+    fetch("http://localhost:8080/products")
+      .then(response => response.json())
+      .then(productsData => {
+        this.setState({
+          productsLoading: false,
+          products: productsData
+        });
+      })
+      .catch(() => {
+        console.log("Failed to retrieve products for SearchSidebar");
+        this.setState({ productsError: true });
+      }); //end of fetch
 
+    /*********************************************************************************
+     * Note: region most likely to fetched in main component and passed down in props
+     *********************************************************************************/
+    //fetch regions name
+    fetch("http://localhost:8080/regions")
+      .then(response => response.json())
+      .then(regionsData => {
+        const regionNames = regionsData.map(region => {
+          return region.name;
+        });
+        this.setState({
+          regionsloading: false,
+          regions: regionNames
+        });
+      })
+      .catch(() => {
+        console.log("Failed to retrieve products for SearchSidebar");
+        this.setState({ regionsError: true });
+      }); //end of fetch
+  }
 
-		 /*********************************************************************************
-		  * Note: region most likely to fetched in main component and passed down in props
-		  *********************************************************************************/
-		 //fetch regions name
-		 fetch("http://localhost:8080/regions")
-		 .then(response => response.json())
-		 .then(regionsData => {
-			 const regionNames = regionsData.map(region => {
-				return region.name;
-			 });
-			 this.setState({
-				 regionsloading: false,
-				 regions: regionNames
-				});
-		 })
-		 .catch(() => {
-			 console.log("Failed to retrieve products for SearchSidebar");
-			 this.setState({regionsError: true});
-		 });//end of fetch
+  handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    type === "checkbox"
+      ? this.setState({ [name]: checked })
+      : this.setState({ [name]: value });
+  }
 
-		 
-	}
+  render() {
+    const searchProducts = this.state.productsError
+      ? "Failed to load. Please try again later."
+      : this.state.productsLoading
+      ? "loading..."
+      : this.state.products.map(product => {
+          return (
+            <SearchItem
+              key={product.prod_id}
+              value={product.prod_id}
+              className="products"
+              name={product.name}
+              handleChange={this.handleChange}
+            />
+          );
+        });
 
-	handleChange(event) {
-		
-        const {name, value, type, checked} = event.target
-        type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value });
-	}
-	
-	render() {
-		const searchProducts = (this.state.productsError) ? "Failed to load. Please try again later." : 
-			(this.state.productsLoading) ? "loading..." : this.state.products.map(product => {
-				return <SearchItem key={product.prod_id} value={product.prod_id} className="products" name={product.name} handleChange={this.handleChange}/>
-			});
+    const searchRegions = this.state.regionsError
+      ? "Failed to load. Please try again later."
+      : this.state.regionsloading
+      ? "loading..."
+      : this.state.regions.map(region => {
+          return (
+            <SearchItem
+              key={region}
+              value={region}
+              className="regions"
+              name={region}
+              handleChange={this.handleChange}
+            />
+          );
+        });
 
-		const searchRegions = (this.state.regionsError) ? "Failed to load. Please try again later." : 
-			(this.state.regionsloading) ? "loading..." : this.state.regions.map(region => {
-				return <SearchItem key={region} value={region} className="regions" name={region} handleChange={this.handleChange}/>
-			});
+    const searchSeasons = this.state.seasonsloading
+      ? "loading..."
+      : this.state.seasons.map((season, index) => {
+          return (
+            <SearchItem
+              key={season}
+              value={season}
+              attributeName="seasons"
+              name={season}
+              handleChange={this.handleChange}
+            />
+          );
+        });
 
-		const searchSeasons = (this.state.seasonsloading) ? "loading..." : this.state.seasons.map((season,index) => {
-			return <SearchItem key={season} value={season} attributeName="seasons" name={season} handleChange={this.handleChange}/>
-		});
+    const searchTypes = this.state.productTypesloading
+      ? "loading..."
+      : this.state.productTypes.map((type, index) => {
+          return (
+            <SearchItem
+              key={type}
+              value={type}
+              attributeName="types"
+              name={type}
+              handleChange={this.handleChange}
+            />
+          );
+        });
+    return (
+      <div>
+        <div>
+          <div
+            style={{
+              display: "inline-block",
+              right: "341px",
+              marginTop: "10px",
+              position: "relative",
+              zIndex: "4"
+            }}
+          >
+            <input type="text" placeholder="Search Here..." />
+          </div>
+          <div className="SearchSidebar">
+            <div id="test">
+              <h1>Products</h1>
+              {searchProducts}
 
-		const searchTypes =  (this.state.productTypesloading) ? "loading..." : this.state.productTypes.map((type,index) => {
-			return <SearchItem key={type} value={type} attributeName="types" name={type} handleChange={this.handleChange}/>
-		});
-		return (
-			<div>
-				<h1>Products</h1>
-				{searchProducts}
-				
-				<h1>Regions</h1>
-				{searchRegions}
+              <h1>Regions</h1>
+              {searchRegions}
 
-				<h1>Seasons</h1>
-				{searchSeasons}
+              <h1>Seasons</h1>
+              {searchSeasons}
 
-				<h1>Types</h1>
-				{searchTypes}
-			</div>
-		);
-	}
-
+              <h1>Types</h1>
+              {searchTypes}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-
 /****************************************************
- * PLEASE LEAVE ONLY (don't remove this section) yet. 
+ * PLEASE LEAVE ONLY (don't remove this section) yet.
  ****************************************************/
-// function SearchSidebar(props) {
-// 	//Search input box
-// 	let [searchText, setsearchText] = useState("");
-	
-// 	const inputText = useRef("");
-	
-// 	/*
-// 		searchCategories = Fetch search options I.E Products, Season, Region. 
-// 			This also include the array of items I.E Products item are [Barley, Leek, Sockeye Salmon], Season: [winter, summer]
->>>>>>> 288584a13d79d39d6f12e97c2b897f04f05dba5a
+//  function SearchSidebar(props) {
+//  	//Search input box
+//  	let [searchText, setsearchText] = useState("");
+
+//  	const inputText = useRef("");
+
+/*
+ 		searchCategories = Fetch search options I.E Products, Season, Region. 
+ 			This also include the array of items I.E Products item are [Barley, Leek, Sockeye Salmon], Season: [winter, summer]
+
 		
-// 		Create an array of search options by "mapping" the fetched categories (searchCategories) 
-// 		using this component: <searchCategory categoryHeader=searchCategory.name itemList=searchCategory.items/>
+ 		Create an array of search options by "mapping" the fetched categories (searchCategories) 
+ 		using this component: <searchCategory categoryHeader=searchCategory.name itemList=searchCategory.items/>
 		
-<<<<<<< HEAD
+
 		Note: 	categoryHeader - the search option category name/header.
 				itemList - an array of the options that you can check mark to display on map 
 					I.E ["Barley", "Wild Leek", "Sockeye Salmon"], ["Winter", "Summer", "Spring"]
 	*/
-  const searchCategories = null;
-=======
-// 		Note: 	categoryHeader - the search option category name/header.
-// 				itemList - an array of the options that you can check mark to display on map 
-// 					I.E ["Barley", "Wild Leek", "Sockeye Salmon"], ["Winter", "Summer", "Spring"]
-// 	*/
-// 	const searchCategories = null;
-	
-// 	const autoCompleteComponents = null;
-// 	useEffect( () => {
-// 		// autoCompleteComponents = update the autoComplete feature I.E searchText="B", list of anything that starts with 'b' and case-insensitive.
-// 	}, [searchText]);
-	
-	
-// 	const [selectedFilterProduct, setselectedFilterProduct] = useState(null);
-	
-// 	return (
-// 		<div className="SearchSidebar">
-// 			{/*
-// 				display Search bar and pull tab button.
-// 				<input ref={inputText} type="text" value={searchText}/>
-// 				{autoCompleteComponents}
-// 				{searchCategories}
-// 				{display advanced filter window. <FilterProductWindow product={}/>}
-// 			*/}
-// 			<div id="test">
-				
+//   const searchCategories = null;
+/* 
+ 		Note: 	categoryHeader - the search option category name/header.
+ 				itemList - an array of the options that you can check mark to display on map 
+ 					I.E ["Barley", "Wild Leek", "Sockeye Salmon"], ["Winter", "Summer", "Spring"]
+ 	*/
+// const searchCategories = null;
+
+//  	const autoCompleteComponents = null;
+//  	useEffect( () => {
+//  		// autoCompleteComponents = update the autoComplete feature I.E searchText="B", list of anything that starts with 'b' and case-insensitive.
+//  	}, [searchText]);
+
+//  	const [selectedFilterProduct, setselectedFilterProduct] = useState(null);
+
+//  	return (
+//  		<div className="SearchSidebar">
+//  			{/*
+//  				display Search bar and pull tab button.
+//  				<input ref={inputText} type="text" value={searchText}/>
+//  				{autoCompleteComponents}
+//  				{searchCategories}
+//  				{display advanced filter window. <FilterProductWindow product={}/>}
+//  			*/}
+//  			<div id="test">
+
 // 			</div>
-			
-// 		</div>
-// 	);
-	
+
+//  		</div>
+//  	);
+
 // }
->>>>>>> 288584a13d79d39d6f12e97c2b897f04f05dba5a
 
-  const autoCompleteComponents = null;
-  useEffect(() => {
-    // autoCompleteComponents = update the autoComplete feature I.E searchText="B", list of anything that starts with 'b' and case-insensitive.
-  }, [searchText]);
+//   const autoCompleteComponents = null;
+//   useEffect(() => {
+//     // autoCompleteComponents = update the autoComplete feature I.E searchText="B", list of anything that starts with 'b' and case-insensitive.
+//   }, [searchText]);
 
-  const [selectedFilterProduct, setselectedFilterProduct] = useState(null);
+//   const [selectedFilterProduct, setselectedFilterProduct] = useState(null);
 
-  return (
-    <div>
-      {" "}
-      <div
-        style={{ display: "inline-block", right: "341px",marginTop:"10px", position: "relative",zIndex:"4" }}
-      >
-        <input type="text" placeholder="Search Here..." />
-      </div>
-      <div className="SearchSidebar">
-        <div id="test"></div>
-      </div>
-    </div>
-  );
-}
+//   return (
+
+//   );
+// }
 
 export default SearchSidebar;
